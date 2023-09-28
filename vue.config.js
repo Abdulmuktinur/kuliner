a203074cfe73d9c170e06335ade01ea3f49cd54d
Abdul Mukti Nurrohman <abdulmuktinurrohman@gmail.com>
2023-09-28 19:50:07 +0700

    fix(Updated Upstream): update file

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,6 @@
-<<<<<<< Updated upstream
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 
   publicPath: "/kuliner/",
-=======
-// vue.config.js
-const { defineConfig } = require("@vue/cli-service");
-
-module.exports = defineConfig({
-  // options...
-  publicPath: process.env.NODE_ENV === "production" ? "/kuliner/" : "/",
->>>>>>> Stashed changes
 });
